0dfbb9c6c00c73dab44881c626520480a0ebe614
zxj <775718540@qq.com>
2024-12-11 16:24:39 +0800

    Q134

---
+++ src/main/java/org/kubo/leetcode/贪心算法/Q134加油站/Solution.java
@@ -0,0 +1,20 @@
+package org.kubo.leetcode.贪心算法.Q134加油站;
+
+import org.kubo.Utils.ParseUtils;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(canCompleteCircuit(
+                ParseUtils.strToIntArr("[1,2,3,4,5]")
+                , ParseUtils.strToIntArr("[1,2,3,4,5]")));
+    }
+
+    /**
+     *
+     */
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        return 0;
+    }
+
+}
