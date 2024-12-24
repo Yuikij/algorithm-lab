982a9f1d9056eff5bdbb61a2708c82501c5860f5
shiori <775718540@qq.com>
2024-12-25 00:22:31 +0800

    Q509

---
+++ src/main/java/org/kubo/leetcode/动态规划/Q509斐波那契数/Solution.java
@@ -0,0 +1,17 @@
+package org.kubo.leetcode.动态规划.Q509斐波那契数;
+
+import org.kubo.Utils.ParseUtils;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(fib(3));
+    }
+
+    /**
+     * 思路：问题转换
+     */
+    public static int fib(int n) {
+        return 0;
+    }
+}
