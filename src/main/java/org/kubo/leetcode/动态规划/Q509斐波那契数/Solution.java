fb4bf629ce5db73199776e4246d64b391cfe62b5
shiori <775718540@qq.com>
2024-12-27 01:58:05 +0800

    Q509

--- src/main/java/org/kubo/leetcode/动态规划/Q509斐波那契数/Solution.java
+++ src/main/java/org/kubo/leetcode/动态规划/Q509斐波那契数/Solution.java
@@ -1,13 +1,15 @@
 package org.kubo.leetcode.动态规划.Q509斐波那契数;
 
+import java.util.Arrays;
+
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(fib(4));
+        System.out.println(fib2(4));
     }
 
     /**
-     * 思路：
+     * 递归，并且有重复的计算
      */
     public static int fib(int n) {
         if (n == 0) {
@@ -17,5 +19,17 @@ public class Solution {
             return 1;
         }
         return fib(n - 1) + fib(n - 2);
+    }
+
+
+    public static int fib2(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[n];
     }
 }
