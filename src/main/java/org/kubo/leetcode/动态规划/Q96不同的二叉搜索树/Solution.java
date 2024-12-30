8c6c109a5e6bd0e756661fb0b2d696873a393473
zxj <775718540@qq.com>
2024-12-30 17:24:36 +0800

    Q96

---
+++ src/main/java/org/kubo/leetcode/动态规划/Q96不同的二叉搜索树/Solution.java
@@ -0,0 +1,15 @@
+package org.kubo.leetcode.动态规划.Q96不同的二叉搜索树;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(numTrees(2));
+    }
+
+    /**
+     * 思路：
+     */
+    public static int numTrees(int n) {
+        return 0;
+    }
+}
