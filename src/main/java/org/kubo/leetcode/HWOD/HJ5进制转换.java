29cd462ebc605797009fb2bc217618d766476852
zxj <775718540@qq.com>
2025-02-26 10:06:56 +0800

    fix(HWOD): 修复 HJ5 进制转换代码- 修复了代码中的逻辑错误，现在可以正确处理输入的十六进制数字 - 优化了代码结构，提高了可读性和可维护性 - 添加了对数字字符的处理，确保正确转换为对应的十进制值

--- src/main/java/org/kubo/leetcode/HWOD/HJ5进制转换.java
+++ src/main/java/org/kubo/leetcode/HWOD/HJ5进制转换.java
@@ -1,27 +1,31 @@
 package org.kubo.leetcode.HWOD;
+
 import java.util.Scanner;
 
 
 public class HJ5进制转换 {
     public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            // 注意 hasNext 和 hasNextLine 的区别
-            while (in.hasNextInt()) { // 注意 while 处理多个 case
-                String str = in.nextLine();
-                str = str.substring(2,str.length());
-                int sum = 0;
-                for (int index = 0; index<str.length(); index++) {
-                    int n = (int)str.charAt(index);
-                    if(n>=65){
-                        n = n-54;
-                    }
-
-                    if (n>97) {
-                        n=n-86;
-                    }
-                    sum +=  Math.pow(16, str.length()-index-1);
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNext()) { // 注意 while 处理多个 case
+            String str = in.nextLine();
+            str = str.substring(2);
+            int sum = 0;
+            for (int index = 0; index < str.length(); index++) {
+                int n = str.charAt(index);
+                if (n >= 48 && n <= 57) {
+                    n = n - 48;
                 }
-                System.out.println(sum);
+                if (n >= 65) {
+                    n = n - 54;
+                }
+
+                if (n > 97) {
+                    n = n - 86;
+                }
+                sum += (int) Math.pow(16, str.length() - index - 1) * n;
             }
+            System.out.println(sum);
         }
+    }
 }
